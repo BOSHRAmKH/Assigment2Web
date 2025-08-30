1fb285183efde2ac11446d947cc908619a25a1b9
Pc World <boshra.m.kh2020@gmail.com@gmail.com>
2025-08-30 20:38:33 +0300

    first commit

---
+++ githhubassig2web2.php
@@ -0,0 +1,14 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My Project Submission</title>
+</head>
+<body>
+    <h2>Click the link to view the project on GitHub:</h2>
+    <a href="https://github.com/BOSHRAmKH/Assigment2Web/blob/main/githhubassig2web2" target="_blank">Click here</a>
+</body>
+</html>
